5556fd98e2df6f882d9a036b0456b326aad9e6fc
Boris Zhang <bozhang@canva.com>
2024-08-21 10:25:57 +1000

    update

--- bin/buildkite-agent.py
+++ bin/buildkite-agent.py
@@ -1,8 +1,12 @@
 #! /nix/var/nix/profiles/default/bin/nix-shell
-#! nix-shell python3.nix -i python3
+# ! nix-shell python3.nix -i python3
+import fnmatch
+import json
 import os
 import subprocess
 import sys
+import tempfile
+from io import BytesIO
 from typing import Any
 
 from ruamel.yaml import YAML
@@ -30,8 +34,23 @@ def _search_for_real_buildkite_agent() -> str:
     raise Exception('buildkite-agent not found!')
 
 
+# BUILDKITE_PLUGINS="[{\"github.com/bz-canva/buildkite-agent-wrapper-buildkite-plugin#main\":
+# {\"trusted_branches\":[\"master\",\"release-*\",\"boris-trusted*\"]}}, ... }]"
+def _get_trusted_branches(json_str: str) -> list[str]:
+    json_data = json.loads(json_str)
+    plugin_config: dict[str, Any] = next(
+        (key for key in json_data if "buildkite-agent-wrapper-buildkite-plugin" in key))
+    return plugin_config.get("trusted_branches")
+
+
 def _is_trusted_job() -> bool:
-    return True
+    current_branch = _require_env("BUILDKITE_BRANCH")
+    trusted_branch = _get_trusted_branches(_require_env("BUILDKITE_PLUGINS"))
+
+    for pattern in trusted_branch:
+        if fnmatch.fnmatch(current_branch, pattern):
+            return True
+    return False
 
 
 def _inject_into_steps(steps: list[Any]):
@@ -39,14 +58,14 @@ def _inject_into_steps(steps: list[Any]):
         if isinstance(step, dict) and "agents" in step:
             agents = step["agents"]
             if isinstance(agents, list):
-                agents.append("trusted=true")
+                agents.append(f"trusted={str(_is_trusted_job()).lower()}")
             elif isinstance(agents, dict):
-                agents["trusted"] = True
+                agents["trusted"] = _is_trusted_job()
             else:
                 raise Exception(f"Not supported: {agents}")
 
 
-def _inject_trusted_tags_into_pipeline_yaml(pipeline_yaml: str):
+def _inject_trusted_tags_into_pipeline_yaml(pipeline_yaml: str) -> str:
     yaml = YAML()
     pipeline_data = yaml.load(pipeline_yaml)
     # if current branch is trusted branch, add 'trusted=true', else ass 'trusted=false'
@@ -55,21 +74,33 @@ def _inject_trusted_tags_into_pipeline_yaml(pipeline_yaml: str):
     else:
         _inject_into_steps(pipeline_data.get("steps", []))
 
+    buf = BytesIO()
+    yaml.dump(pipeline_data, buf)
+    return buf.getvalue().decode("utf-8")
+
+
+def _require_env(name: str) -> str:
+    if (value := os.getenv(name)) is None:
+        raise Exception(f"Env {name} doesn't exist!")
+    return value
+
 
 def _run_inherit_io(args: list[str]) -> int:
+    print(f"Running: {args}")
     result = subprocess.run(args, stdout=sys.stdout, stderr=sys.stderr)
     return result.returncode
 
 
 def main():
-    print(f"args: {sys.argv}")
     if len(sys.argv) > 3 and sys.argv[1:3] == ['pipeline', 'upload']:
         pipeline_file = sys.argv[3]
-        print(_read_file_to_str(pipeline_file))
+        yaml = _inject_trusted_tags_into_pipeline_yaml(_read_file_to_str(pipeline_file))
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+            temp_file.write(yaml)
+            _run_inherit_io([_search_for_real_buildkite_agent()] + sys.argv[1:3] + [temp_file.name] + sys.argv[3:])
     else:
         _run_inherit_io([_search_for_real_buildkite_agent()] + sys.argv[1:])
 
 
 if __name__ == "__main__":
-    print(f"config: {os.getenv('BUILDKITE_PLUGIN_CONFIGURATION')}")
     main()
